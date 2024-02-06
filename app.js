fa8cc1019312459dd7c12b5e19ff702c9ad9c4f1
Brian Tochterman <btoch23@gmail.com>
2024-02-06 13:02:51 -0600

    navbar, boilerplate, ejs-mate

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const Problem = require('./models/problem');
 
+mongoose.connect('mongodb://localhost:27017/sendSheet');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.render('home')
