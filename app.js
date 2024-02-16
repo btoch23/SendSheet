df879886af91c0a49e8f02e00619313f363e245f
Brian Tochterman <btoch23@gmail.com>
2024-02-16 12:28:00 -0600

    configure passport local strategy

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const flash = require('connect-flash');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+const statsRouter = require('./routes/statsRouter');
+const problemsRouter = require('./routes/problemsRouter');
 
 mongoose.connect('mongodb://localhost:27017/sendSheet');
 
@@ -18,9 +24,6 @@ db.once('open', () => {
 
 const app = express();
 
-const statsRouter = require('./routes/statsRouter');
-const problemsRouter = require('./routes/problemsRouter');
-
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +32,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(flash());
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
@@ -42,6 +44,15 @@ const sessionConfig = {
     }
 };
 app.use(session(sessionConfig));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
