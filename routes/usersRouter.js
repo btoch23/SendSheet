a5020a12cd910e22b5ff63efe64bf1065b815fba
Brian Tochterman <btoch23@gmail.com>
2024-02-16 13:05:43 -0600

    login routes and form

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+const passport = require('passport')
 
 const usersRouter = express.Router();
 
@@ -7,8 +9,26 @@ usersRouter.route('/register')
 .get((req, res) => {
     res.render('users/register')
 })
-.post(async(req, res) => {
-    res.send(req.body);
+.post(catchAsync(async(req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to SendSheet!');
+        res.redirect('/problems');  
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}))
+
+usersRouter.route('/login')
+.get((req, res) => {
+    res.render('users/login')
+})
+.post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/problems');
 })
 
 module.exports = usersRouter;
